281e171dd1b5fc3ab9919307bb19773f888dde80
Parash Agrawal <parash.a@namespace.jp>
2021-01-06 16:45:17 +0545

    Added simple list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div>
-      Hello World
+      <input type="checkbox" /><span className="bg-red">One </span> <br />
+      <input type="checkbox" /><span className="bg-red">Two </span> <br />
+      <input type="checkbox" /><span className="bg-red">Three </span> <br />
+      <input type="checkbox" /><span className="bg-red">Four </span> <br /> 
     </div>
   );
 }
